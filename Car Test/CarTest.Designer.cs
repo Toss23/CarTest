278737af3b85ff6f0fbb7f91540924a83595b951
Alexander <95612872+Toss23@users.noreply.github.com>
2023-06-10 20:04:26 +0300

    Initial commit

---
+++ Car Test/CarTest.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Car_Test
+{
+    partial class CarTest
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel = new System.Windows.Forms.Panel();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_delete = new System.Windows.Forms.Button();
+            this.label_info = new System.Windows.Forms.Label();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_end = new System.Windows.Forms.Button();
+            this.label_iteration = new System.Windows.Forms.Label();
+            this.label_step = new System.Windows.Forms.Label();
+            this.label_best = new System.Windows.Forms.Label();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label_speed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(13, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(870, 724);
+            this.panel.TabIndex = 0;
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(892, 12);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(221, 27);
+            this.button_add.TabIndex = 1;
+            this.button_add.Text = "Add Sign";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_delete
+            // 
+            this.button_delete.Location = new System.Drawing.Point(892, 45);
+            this.button_delete.Name = "button_delete";
+            this.button_delete.Size = new System.Drawing.Size(221, 27);
+            this.button_delete.TabIndex = 2;
+            this.button_delete.Text = "Delete Sign";
+            this.button_delete.UseVisualStyleBackColor = true;
+            this.button_delete.Click += new System.EventHandler(this.button_delete_Click);
+            // 
+            // label_info
+            // 
+            this.label_info.AutoSize = true;
+            this.label_info.Location = new System.Drawing.Point(889, 87);
+            this.label_info.Name = "label_info";
+            this.label_info.Size = new System.Drawing.Size(54, 13);
+            this.label_info.TabIndex = 3;
+            this.label_info.Text = "Позиция:";
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(892, 231);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(221, 27);
+            this.button_start.TabIndex = 4;
+            this.button_start.Text = "Start Learn";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_end
+            // 
+            this.button_end.Location = new System.Drawing.Point(892, 264);
+            this.button_end.Name = "button_end";
+            this.button_end.Size = new System.Drawing.Size(221, 27);
+            this.button_end.TabIndex = 5;
+            this.button_end.Text = "End Learn";
+            this.button_end.UseVisualStyleBackColor = true;
+            this.button_end.Click += new System.EventHandler(this.button_end_Click);
+            // 
+            // label_iteration
+            // 
+            this.label_iteration.AutoSize = true;
+            this.label_iteration.Location = new System.Drawing.Point(889, 303);
+            this.label_iteration.Name = "label_iteration";
+            this.label_iteration.Size = new System.Drawing.Size(109, 13);
+            this.label_iteration.TabIndex = 6;
+            this.label_iteration.Text = "Номер итерации = 0";
+            // 
+            // label_step
+            // 
+            this.label_step.AutoSize = true;
+            this.label_step.Location = new System.Drawing.Point(889, 331);
+            this.label_step.Name = "label_step";
+            this.label_step.Size = new System.Drawing.Size(90, 13);
+            this.label_step.TabIndex = 7;
+            this.label_step.Text = "Текущий ход = 0";
+            // 
+            // label_best
+            // 
+            this.label_best.AutoSize = true;
+            this.label_best.Location = new System.Drawing.Point(889, 358);
+            this.label_best.Name = "label_best";
+            this.label_best.Size = new System.Drawing.Size(101, 13);
+            this.label_best.TabIndex = 8;
+            this.label_best.Text = "Лучший проход = 0";
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(892, 452);
+            this.trackBar.Maximum = 9;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(221, 45);
+            this.trackBar.TabIndex = 9;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label_speed
+            // 
+            this.label_speed.AutoSize = true;
+            this.label_speed.Location = new System.Drawing.Point(889, 436);
+            this.label_speed.Name = "label_speed";
+            this.label_speed.Size = new System.Drawing.Size(102, 13);
+            this.label_speed.TabIndex = 10;
+            this.label_speed.Text = "Скорость: 1000 мс";
+            // 
+            // CarTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 748);
+            this.Controls.Add(this.label_speed);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.label_best);
+            this.Controls.Add(this.label_step);
+            this.Controls.Add(this.label_iteration);
+            this.Controls.Add(this.button_end);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.label_info);
+            this.Controls.Add(this.button_delete);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "CarTest";
+            this.Text = "CarTest";
+            this.Load += new System.EventHandler(this.CarTest_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_delete;
+        private System.Windows.Forms.Label label_info;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_end;
+        private System.Windows.Forms.Label label_iteration;
+        private System.Windows.Forms.Label label_step;
+        private System.Windows.Forms.Label label_best;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label_speed;
+    }
+}
+
---
+++ Car Test/CarTest.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Car_Test
+{
+    public partial class CarTest : Form
+    {
+        // Размеры картинок дорог
+        private const int ROAD_SIZE = 32;
+
+        // База данных SQLite
+        private SQLiteConnection _connection = new SQLiteConnection("Data Source=database.db");
+        private SQLiteCommand _command = new SQLiteCommand();
+
+        // Картинки дорог
+        private Bitmap[] _roadBitmaps = new Bitmap[]
+        {
+            null,
+            Properties.Resources.Road,
+            Properties.Resources.Road,
+            Properties.Resources.Road,
+            Properties.Resources.Road,
+            Properties.Resources.Road_Cross,
+            Properties.Resources.Start,
+            Properties.Resources.Finish
+        };
+
+        // Картинки машин
+        private Bitmap[] _carBitmaps = new Bitmap[]
+        {
+            Properties.Resources.Car_Top,
+            Properties.Resources.Car_Right,
+            Properties.Resources.Car_Bottom,
+            Properties.Resources.Car_Left
+        };
+
+        // Массив дороги
+        private int[,] _roadmap = {
+            {0,0,0,0,2,1,0,0,0,0,0,0,0,0,2,7,0,0,0,0 },
+            {0,0,0,0,2,1,0,0,0,0,0,0,0,0,2,1,0,0,0,0 },
+            {0,0,0,0,5,5,4,4,5,5,4,4,4,4,5,5,4,4,5,5 },
+            {0,0,0,0,5,5,3,3,5,5,3,3,3,3,5,5,3,3,5,5 },
+            {0,0,0,0,0,0,0,0,2,1,0,0,0,0,2,1,0,0,2,1 },
+            {0,0,0,0,0,0,0,0,2,1,0,0,0,0,2,1,0,0,2,1 },
+            {0,0,5,5,4,4,4,4,5,5,4,4,4,4,5,5,4,4,5,5 },
+            {0,0,5,5,3,3,3,3,5,5,3,3,3,3,5,5,3,3,5,5 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,2,1 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,2,1 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,2,1 },
+            {0,0,5,5,4,4,4,4,5,5,4,4,4,4,5,5,4,4,5,5 },
+            {0,0,5,5,3,3,3,3,5,5,3,3,3,3,5,5,3,3,5,5 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,0,0 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,0,0 },
+            {0,0,2,1,0,0,0,0,2,1,0,0,0,0,2,1,0,0,0,0 },
+            {0,0,5,5,4,4,4,4,5,5,4,4,4,4,5,5,4,4,4,4 },
+            {0,0,5,5,3,3,3,3,5,5,3,3,3,3,5,5,3,3,3,3 },
+            {0,0,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,2,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+        };
+
+        private int[,] _objects = {
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+            {0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+        };
+
+        // Координаты выделенной дороги
+        private int[] _selectedRoadPosition = new int[] { -1, -1 };
+
+        // Позиция машины
+        private int[] _carPosition = new int[] { 3, 19, 0 };
+
+        // Обучение
+        private int[] _lastCross = new int[2];
+        private int _iteration = 0;
+        private int _step = 0;
+        private int _bestResult = 0;
+
+        public CarTest()
+        {
+            InitializeComponent();
+
+            // База данных
+            _connection.Open();
+            _command.Connection = _connection;
+        }
+
+        private void CarTest_Load(object sender, EventArgs e)
+        {
+            TransposeArrays(); // Необходимо перевернуть массивы
+            BuildRoad(); // Построение карты
+            DrawRoad(); // Отрисовка объектов и дороги
+        }
+
+        private void TransposeArrays()
+        {
+            int[,] roadmap = new int[20, 20];
+            int[,] objects = new int[20, 20];
+            for (int x = 0; x < 20; x++)
+            {
+                for (int y = 0; y < 20; y++)
+                {
+                    roadmap[x, y] = _roadmap[y, x];
+                    objects[x, y] = _objects[y, x];
+                }
+            }
+            _roadmap = roadmap;
+            _objects = objects;
+        }
+
+        private void BuildRoad()
+        {
+            for (int x = 0; x < 20; x++)
+            {
+                for (int y = 0; y < 20; y++)
+                {
+                    // Создание картинки дороги
+                    PictureBox road = new PictureBox
+                    {
+                        Name = $"{x}_{y}", // Имя
+                        Size = new Size(ROAD_SIZE, ROAD_SIZE), // Размеры
+                        Location = new Point(ROAD_SIZE * x, ROAD_SIZE * y), // Позиция
+                    };
+
+                    // Привязка событий
+                    road.Click += RoadClick; // Выделение при нажатии
+                    road.MouseHover += RoadHover; // Отображение координатов блока
+
+                    // Заполение картинками панели
+                    panel.Controls.Add(road);
+                }
+            }
+        }
+
+        private void DrawRoad()
+        {
+            for (int x = 0; x < 20; x++)
+            {
+                for (int y = 0; y < 20; y++)
+                {
+                    // Доступ к картинке дороги
+                    PictureBox road = panel.Controls[x + "_" + y] as PictureBox;
+                    road.BackgroundImage = _roadBitmaps[_roadmap[x, y]];
+
+                    // Поворот картинки
+                    switch (_roadmap[x, y])
+                    {
+                        case 2:
+                            road.BackgroundImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                            break;
+                        case 3:
+                            road.BackgroundImage = RotateImage(road.BackgroundImage, 90);
+                            break;
+                        case 4:
+                            road.BackgroundImage = RotateImage(road.BackgroundImage, 270);
+                            break;
+                    }
+
+                    // Установка кирпичей и машины
+                    switch (_objects[x, y])
+                    {
+                        case 1:
+                            road.Image = Properties.Resources.Block;
+                            break;
+                        case 2:
+                            road.Image = Properties.Resources.Car_Top;
+                            break;
+                    }
+                }
+            }
+        }
+
+        // Поворот картинки
+        private Image RotateImage(Image image, float angle)
+        {
+            Bitmap rotatedImage = new Bitmap(image.Width, image.Height);
+            using (Graphics g = Graphics.FromImage(rotatedImage))
+            {
+                g.TranslateTransform(image.Width / 2, image.Height / 2);
+                g.RotateTransform(angle);
+                g.DrawImage(image, -image.Width / 2, -image.Height / 2, image.Width, image.Height);
+            }
+            return rotatedImage;
+        }
+
+        private void RoadClick(object sender, EventArgs e)
+        {
+            PictureBox road = sender as PictureBox;
+            string[] roadPosition = road.Name.Split('_');
+            int x = int.Parse(roadPosition[0]);
+            int y = int.Parse(roadPosition[1]);
+
+            // Удаление рамок на предыдущей выделенной дороге
+            if (_selectedRoadPosition[0] > 0)
+            {
+                PictureBox selectedRoad = panel.Controls[_selectedRoadPosition[0] + "_" + _selectedRoadPosition[1]] as PictureBox;
+                selectedRoad.BorderStyle = BorderStyle.None;
+            }
+
+            // Выделение дороги
+            road.BorderStyle = BorderStyle.Fixed3D;
+            _selectedRoadPosition[0] = x;
+            _selectedRoadPosition[1] = y;
+        }
+
+        private void RoadHover(object sender, EventArgs e)
+        {
+            // Отображение координатов
+            PictureBox road = sender as PictureBox;
+            label_info.Text = "Позиция: " + road.Name;
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            int x = _selectedRoadPosition[0];
+            int y = _selectedRoadPosition[1];
+
+            if (x != -1)
+            {
+                if (_roadmap[x, y] >= 1 & _roadmap[x, y] <= 4)
+                {
+                    PictureBox selectedRoad = panel.Controls[x + "_" + y] as PictureBox;
+                    selectedRoad.Image = Properties.Resources.Block;
+                    _objects[x, y] = 1;
+                }
+            }
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            int x = _selectedRoadPosition[0];
+            int y = _selectedRoadPosition[1];
+
+            if (x != -1)
+            {
+                PictureBox road = panel.Controls[x + "_" + y] as PictureBox;
+                road.Image = null;
+                _objects[x, y] = 0;
+            }
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void button_end_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            StopMove(false);
+        }
+
+        private void StopMove(bool result)
+        {
+            // Удаление машины
+            PictureBox car = panel.Controls[_carPosition[0] + "_" + _carPosition[1]] as PictureBox;
+            car.Image = null;
+            _carPosition = new int[3] { 3, 19, 0 };
+
+            // Установка машины на старт
+            car = panel.Controls[_carPosition[0] + "_" + _carPosition[1]] as PictureBox;
+            car.Image = Properties.Resources.Car_Top;
+
+            // Запись результата в БД
+            _command.CommandText = "INSERT INTO Runs (Run_result, Run_time) VALUES ('" + result + "', " + _step + ")";
+            _command.ExecuteNonQuery();
+
+            _iteration++;
+            label_iteration.Text = "Номер итерации = " + _iteration.ToString();
+            _step = 0;
+        }
+
+        private void trackBar_Scroll(object sender, EventArgs e)
+        {
+            timer.Interval = (10 - trackBar.Value) * 100;
+            label_speed.Text = "Скорость: " + timer.Interval + "мс";
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            MoveCar();
+            _step++;
+            label_step.Text = "Текущий ход = " + _step.ToString();
+        }
+
+        private void MoveCar()
+        {
+            // Координаты машины
+            int x = _carPosition[0];
+            int y = _carPosition[1];
+            int direction = _carPosition[2];
+
+            // Текущая дорога
+            PictureBox roadStart = panel.Controls[x + "_" + y] as PictureBox;
+
+            // Смотрим следующие координаты
+            ChangePosition(direction, ref x, ref y);
+
+            // Проверяем есть ли дорога прямо
+            if (x < 20 && y < 20 && x >= 0 && y >= 0)
+            {
+                // Провереям есть ли знаки на дороге
+                if (_roadmap[x, y] != 0 && _objects[x, y] == 0 && _roadmap[x, y] != 6)
+                {
+                    // Получаям картинку следующей дороги
+                    PictureBox roadEnd = panel.Controls[x + "_" + y] as PictureBox;
+
+                    // Обновляем положение картинки
+                    roadStart.Image = null;
+                    roadEnd.Image = _carBitmaps[direction];
+
+                    // Записываем координаты выезда с последнего прекрестка
+                    if (_roadmap[_carPosition[0], _carPosition[1]] == 5)
+                    {
+                        if (_roadmap[x, y] != 5)
+                        {
+                            _lastCross[0] = x;
+                            _lastCross[1] = y;
+                        }
+                    }
+
+                    // Обновляем координаты
+                    _carPosition = new int[] { x, y, direction };
+
+                    // Проверяем на наличие финиша
+                    if (_roadmap[x, y] == 7)
+                    {
+                        if (_bestResult == 0 || _step < _bestResult)
+                        {
+                            _bestResult = _step;
+                            label_best.Text = "Лучший проход = " + _bestResult.ToString();
+                        }
+                        StopMove(true);
+                    }
+                }
+                else
+                {
+                    // Если машина на перекрестке
+                    if (_roadmap[_carPosition[0], _carPosition[1]] == 5)
+                    {
+                        // Пробуем повернуть направо
+                        x = _carPosition[0];
+                        y = _carPosition[1];
+                        direction = RotateRight(direction, 1);
+                        ChangePosition(direction, ref x, ref y);
+
+                        if (CompareRotation(direction, _roadmap[x, y]))
+                        {
+                            _carPosition[2] = direction;
+                            roadStart.Image = _carBitmaps[direction];
+                        }
+                        else
+                        {
+                            // Пробуем повернуть налево
+                            direction = RotateRight(direction, 2);
+                            x = _carPosition[0];
+                            y = _carPosition[1];
+                            ChangePosition(direction, ref x, ref y);
+                            ChangePosition(direction, ref x, ref y);
+
+                            if (CompareRotation(direction, _roadmap[x, y]))
+                            {
+                                _carPosition[2] = direction;
+                                roadStart.Image = _carBitmaps[direction];
+                            }
+                            else
+                            {
+                                x = _carPosition[0];
+                                y = _carPosition[1];
+                                StopMove(false);
+                                SetBlock(x, y);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        StopMove(false);
+                        SetBlockOnCross();
+                    }
+                }
+            }
+            else
+            {
+                StopMove(false);
+                SetBlockOnCross();   
+            }
+        }
+
+        private bool CompareRotation(int directionCar, int directionRoad)
+        {
+            switch (directionCar)
+            {
+                case 0:
+                    return (directionRoad == 1 || directionRoad == 5);
+                case 1:
+                    return (directionRoad == 3 || directionRoad == 5);
+                case 2:
+                    return (directionRoad == 2 || directionRoad == 5);
+                case 3:
+                    return (directionRoad == 4 || directionRoad == 5);
+            }
+            return false;
+        }
+
+        private void ChangePosition(int direction, ref int x, ref int y)
+        {
+            // 0 - Вверх
+            // 1 - Вправо
+            // 2 - Вниз
+            // 3 - Влево
+            switch (direction)
+            {
+                case 0:
+                    y--;
+                    break;
+                case 1:
+                    x++;
+                    break;
+                case 2:
+                    y++;
+                    break;
+                case 3:
+                    x--;
+                    break;
+            }
+        }
+
+        private int RotateRight(int direction, int count)
+        {
+            direction += count;
+            while (direction >= 4)
+            {
+                direction -= 4;
+            }
+
+            return direction;
+        }
+
+        private void SetBlockOnCross()
+        {
+            _roadmap[_lastCross[0], _lastCross[1]] = 6;
+            PictureBox road = panel.Controls[_lastCross[0] + "_" + _lastCross[1]] as PictureBox;
+            road.Image = Properties.Resources.Block_AI;
+        }
+
+        private void SetBlock(int x, int y)
+        {
+            _roadmap[x, y] = 6;
+            PictureBox road = panel.Controls[x + "_" + y] as PictureBox;
+            road.Image = Properties.Resources.Block_AI;
+        }
+    }
+}
